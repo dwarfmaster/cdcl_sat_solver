ff6e0d343fac8321440549dd1d05ec5f646514f5
lucas8 <luc.linux@mailoo.org>
2016-05-25 21:25:16 +0200

    Add ability to load hipp structures from file

--- hipp.hs
+++ hipp.hs
@@ -1,6 +1,11 @@
 -- vim:set foldmethod=marker:
 
-module HIPP.Structures where
+module HIPP.Structures (Allele (AZero,AUn), Gene (Monozygote,Heterozygote),
+                        Haplotype, Genome, hload)
+                        where
+import System.FilePath
+import System.IO
+import qualified Data.List.Split as Spl
 
 -- {{{ Data structures
 data Allele = AZero | AUn
@@ -40,5 +45,20 @@ poptest = [ [ Monozygote AZero, Monozygote AUn ]
           ]
 -- }}}
 
--- TODO loader
+-- {{{ Loader
+hload :: FilePath -> IO [Genome]
+hload path = do file <- openFile path ReadMode
+                content <- hGetContents file
+                return $ parseLines (lines content)
+
+lineToGenome :: String -> Genome
+lineToGenome line = map rd parts
+ where parts = filter (not.null) $ Spl.splitOn " " line
+       rd c = if      c == "0" then Monozygote AZero
+              else if c == "1" then Monozygote AUn
+              else                  Heterozygote
+
+parseLines :: [String] -> [Genome]
+parseLines = map lineToGenome
+-- }}}
 
