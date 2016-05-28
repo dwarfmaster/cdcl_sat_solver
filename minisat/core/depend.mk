7ac1609cb640417c71d0228fa9e0118da6ae77d9
lucas8 <luc.linux@mailoo.org>
2016-05-28 16:53:00 +0200

    Add minisat source

---
+++ minisat/core/depend.mk
@@ -0,0 +1,29 @@
+/home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.o /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.or /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.od /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.op: \
+ /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.cc \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Sort.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Vec.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/IntTypes.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/XAlloc.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Heap.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Alg.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/utils/Options.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/utils/ParseUtils.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/SolverTypes.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Map.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Alloc.h
+/home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Main.o /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Main.or /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Main.od /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Main.op: \
+ /home/luc/universel/annex/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Main.cc \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/utils/System.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/IntTypes.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/utils/ParseUtils.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/utils/Options.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Vec.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/XAlloc.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Dimacs.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/SolverTypes.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Alg.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Map.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Alloc.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/core/Solver.h \
+ /home/luc/school/prepa/mpe/tipe/cdcl-sat-solver/minisat/mtl/Heap.h
