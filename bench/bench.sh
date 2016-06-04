80383e98fc636d6b26a866bcbcd0b75436dae2c0
lucas8 <luc.linux@mailoo.org>
2016-06-04 11:20:20 +0200

    Change benching executables

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-execs=(./minisat_perso_no ./minisat_vsids_no ./minisat_perso_luby ./minisat_vsids_luby ./minisat_perso_geom ./minisat_vsids_geom)
+execs=(./minisat_perso_no ./minisat_vsids_no ./minisat_vsids_luby ./minisat_vsids_geom ./minisat_pvsids_no ./minisat_pvsids_geom ./minisat_vsids_luby)
 
 for e in ${execs[*]}; do
     for pb in $(ls problems); do
         t1=$(date +%s.%N)
-        timeout 30s $e problems/$pb > /dev/null
+        timeout 60s $e problems/$pb > /dev/null
         t2=$(date +%s.%N)
         dt=$(echo "$t2-$t1" | bc)
         echo "($e,$pb,$dt)"
