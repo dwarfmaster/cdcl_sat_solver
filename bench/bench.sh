28da475d5966def91326ca2f6a8ec1f789f644dc
lucas8 <luc.linux@mailoo.org>
2016-06-06 21:29:28 +0200

    Fixed a bug in bench.sh : wrong minisat name

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-execs=(./minisat_perso_no ./minisat_vsids_no ./minisat_vsids_luby ./minisat_vsids_geom ./minisat_pvsids_no ./minisat_pvsids_geom ./minisat_vsids_luby)
+execs=(./minisat_perso_no ./minisat_vsids_no ./minisat_vsids_luby ./minisat_vsids_geom ./minisat_pvsids_no ./minisat_pvsids_geom ./minisat_pvsids_luby)
 
 for e in ${execs[*]}; do
     for pb in $(ls problems); do
